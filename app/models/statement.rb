81bc217f6b597b824c41b654da9b8d3039aec13e
monkypuzzle <paul.newsam@gmail.com>
2017-06-22 17:47:24 -0700

    Adds routes, view, controller for Statements

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,3 +1,5 @@
 class Statement < ApplicationRecord
   has_many :transactions
+
+  validates :account, :start_date, :end_date, presence: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,5 @@
 class Transaction < ApplicationRecord
   belongs_to :statement
+
+  validates :transaction_date, :description, :amount, :category, :statement_id, presence: true
 end
