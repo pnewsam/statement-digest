0c7c34d0215a62e992743465489df5a2580625e1
monkypuzzle <paul.newsam@gmail.com>
2017-06-22 22:19:54 -0700

    Adds file-upload button on statements#new

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -2,4 +2,8 @@ class StatementsController < ApplicationController
   def index
     @statements = Statement.all
   end
+
+  def create
+    
+  end
 end
--- app/models/statement.rb
+++ app/models/statement.rb
@@ -2,4 +2,5 @@ class Statement < ApplicationRecord
   has_many :transactions
 
   validates :account, :start_date, :end_date, presence: true
+
 end
